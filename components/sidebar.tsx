0aac75112da161735c134956b58750496d45a13f
nipun221 <2oics072@gbu.ac.in>
2023-08-10 23:57:06 +0530

    first commit

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { LayoutDashboard, MessageSquare, ImageIcon, VideoIcon, Music, Code, Settings, GithubIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
+import { FreeCounter } from "@/components/free-counter";
 
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 
@@ -53,7 +54,13 @@ const routes = [
     },
 ];
 
-const Sidebar = () => {
+interface SidebarProps {
+    apiLimitCount: number;
+};
+
+const Sidebar = ({
+    apiLimitCount = 0
+}: SidebarProps) => {
     const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
@@ -97,6 +104,9 @@ const Sidebar = () => {
                 </Link>
             </div>
         </div>
+        <FreeCounter
+            apiLimitCount={apiLimitCount}
+        />
     </div>
   );
 }
